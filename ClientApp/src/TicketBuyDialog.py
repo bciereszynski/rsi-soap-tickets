7be90b448439a97d5505bd70529baaba1121fef3
bciereszynski <b.ciereszynski@wp.pl>
2024-04-22 00:44:10 +0200

    UI ticket buy dialog

--- ClientApp/src/TicketBuyDialog.py
+++ ClientApp/src/TicketBuyDialog.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QLabel, QPushButton, QFrame, QLineEdit
+from zeep import Client
 
 
 class TicketBuyDialog(QDialog):
@@ -7,18 +8,39 @@ class TicketBuyDialog(QDialog):
         super().__init__()
         self.lay = QVBoxLayout()
         self.setWindowModality(Qt.ApplicationModal)
-        buttons = QDialogButtonBox(QDialogButtonBox.Cancel | QDialogButtonBox.Save, Qt.Horizontal)
-        buttons.accepted.connect(self.accept)
-        buttons.rejected.connect(self.reject)
-        self.lay.addWidget(buttons)
+        self.ticketLabel = QLabel("...")
+        self.flightId = None
+        self.lay.addWidget(self.ticketLabel)
+
+        line = QFrame()
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+        self.lay.addWidget(line)
+
+        self.nameEdit = QLineEdit()
+        self.nameEdit.setPlaceholderText("Name")
+        self.lay.addWidget(self.nameEdit)
+
+        self.surnameEdit = QLineEdit()
+        self.surnameEdit.setPlaceholderText("Surname")
+        self.lay.addWidget(self.surnameEdit)
+
+        button = QPushButton("Buy")
+        button.clicked.connect(self.buyFlight)
+        self.lay.addWidget(button)
 
         self.setLayout(self.lay)
 
-    def getItem(self):
-        pass
+    def buyFlight(self):
+        self.client = Client('http://localhost:8080/ticketsWebService-1.0/FlightsServiceService?wsdl')
+        name = self.nameEdit.text()
+        surname = self.surnameEdit.text()
+        result = self.client.service.bookFlight(self.flightId, name, surname)
+        print(result)
+        self.close()
 
-    def setValues(self, values):
-        pass
+    def setFlightStr(self, name):
+        self.ticketLabel.setText(name)
 
-    def resetValues(self):
-        pass
+    def setFlightId(self, identifier):
+        self.flightId = identifier
